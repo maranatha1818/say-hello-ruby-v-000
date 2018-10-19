70c1ccc75fba07a0991ed34d1ed9b10477adc655
Marja Houston <marjahouston@gmail.com>
2018-10-19 17:02:27 +0000

    automatically backed up by learn

--- say_hello.rb
+++ say_hello.rb
@@ -1,4 +1,4 @@
 # Build your say_hello method here
-def say_hello (name,greetting="hello ruby programmer")
-  puts "Hello" +#{name}
-end 
+def say_hello (name,greeting="Ruby Programmer")
+  puts "Hello, #{name}"
+end
